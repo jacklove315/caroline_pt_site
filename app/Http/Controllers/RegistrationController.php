b888279a4e5f5d67ecae1286c42ea9610f469d72
jacklove315 <jacklove315@icloud.com>
2021-10-02 00:28:56 +0100

    fixed auth route to view bug

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -13,7 +13,7 @@ class RegistrationController extends Controller
      */
     public function create()
     {
-        return view('/auth/register');
+        return view('auth.register');
     }
 
     /*
