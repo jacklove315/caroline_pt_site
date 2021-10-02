2e23a8bd606814c968fccce53086ce07ddfe63ea
jacklove315 <jacklove315@icloud.com>
2021-10-02 02:00:34 +0100

    started to use migrations

--- database/migrations/2021_07_15_163012_create_events_table.php
+++ database/migrations/2021_07_15_163012_create_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClassesTable extends Migration
+class CreateEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateClassesTable extends Migration
      */
     public function up()
     {
-        Schema::create('classes', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description');
             $table->string('location');
-            $table->decimal('price');
-            $table->date('date');
-            $table->time('time');
+            $table->integer('day');
+            $table->string('time');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateClassesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('classes');
+        Schema::dropIfExists('events');
     }
 }
--- database/migrations/2021_07_16_120315_create_event_bookings_table.php
+++ database/migrations/2021_07_16_120315_create_event_bookings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMessagesTable extends Migration
+class CreateEventBookingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateMessagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('event_bookings', function (Blueprint $table) {
             $table->id();
-            $table->integer('from')->unsigned();
-            $table->integer('to')->unsigned();
-            $table->text('text');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('class_id');
+            $table->date('event_date');
+            $table->boolean('has_paid_now')->default(0);
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateMessagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('event_bookings');
     }
 }
